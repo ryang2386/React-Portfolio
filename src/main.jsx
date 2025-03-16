971c6ff2af342b2974af728bb12f23cdc768956f
Ryan Gayle <gayler02@nyu.edu>
2025-03-16 18:54:47 -0400

    Importing files into GitHub

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './styles/palette.scss';
+import App from './App';
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Error from './pages/Error';
+
+// using createBrowserRouter to route to each page
+const BrowserRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <AboutMe />,
+            },
+            {
+                path: '/portfolio',
+                element: <Portfolio />,
+            },
+            {
+                path: '/contact',
+                element: <Contact />,
+            },
+            {
+                path: '/resume',
+                element: <Resume />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+  );
