1486721a36ac22fba17ec19e42b44637ffc286de
Ryan Gayle <gayler02@nyu.edu>
2025-03-16 21:20:16 -0400

    updated Contact.jsx by adding a form

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+export default function Contact() {
+    return (
+        <div>
+            <h1>Contact Me</h1>
+            <form>
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" required></input>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required></input>
+                <label for="message">Message:</label>
+                <textarea id="message" name="message" required></textarea>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+};
