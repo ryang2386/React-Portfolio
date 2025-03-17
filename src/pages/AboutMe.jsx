69afa749d0b97dc4d5b5beef27b3d74ef1d8cd1a
Ryan Gayle <gayler02@nyu.edu>
2025-03-16 20:24:10 -0400

    updated AboutMe.jsx with profile pic and short bio

---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,14 @@
+export default function AboutMe() {
+    return (
+        <div>
+            <img src = ".\src\assets\thumbnail_IMG_2844.jpg" alt="Ryan Gayle"/>
+            <h1>About Me</h1>
+            <p>
+                Currently, Ryan Gayle is a SEO Specialist that works for a Sports Merchandising company. Although he enjoys his current career,
+                he is looking to transition into a web or software development role to further his career. At Columbia University Computer Engineering
+                Bootcamp, he is learning the skills necessary to become a Full Stack Developer. He is familiar with HTML, CSS, Javascript, Typescript,
+                and React, and is learning how to use these skills to create full stack applications and websites with optimal functionality.
+            </p>
+        </div>
+    );
+};
