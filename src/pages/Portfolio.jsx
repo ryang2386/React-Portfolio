fcfd1d55e0d91c21d139fce77c20da9dd7e25cfd
Ryan Gayle <gayler02@nyu.edu>
2025-03-18 19:38:28 -0400

    updated Portfolio.jsx with links to six projects with photos included.

---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,21 @@
+export default function Portfolio() { 
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            <section>
+                <a href="https://github.com/ryang2386/Employee-Database"><img src = "src\assets\Screenshot 2025-03-18 19.05.56.png"/></a>
+                <h2>Employee Database</h2>
+                <a href="https://github.com/ryang2386/README-Generator"><img src = "src\assets\Screenshot 2025-03-18 19.03.32.png"/></a>
+                <h2>README Generator</h2>
+                <a href="https://github.com/ryang2386/Vehicle-Builder"><img src = "src\assets\Screenshot 2025-03-18 18.49.36.png"/></a>
+                <h2>Vehicle Builder</h2>
+                <a href="https://github.com/ryang2386/Module-4"><img src = "src\assets\Screenshot 2025-03-18 19.09.05.png"/></a>
+                <h2>Blog Website</h2>
+                <a href="https://github.com/ryang2386/Module-3"><img src = "src\assets\Screenshot 2025-03-18 19.13.31.png"/></a>
+                <h2>Employee Tracker</h2>
+                <a href="https://github.com/ryang2386/MiniProject"><img src = "src\assets\Screenshot 2025-03-18 19.15.25.png"/></a>
+                <h2>CSS Cheat Codes Website</h2>
+            </section>
+        </div>
+    );
+};
