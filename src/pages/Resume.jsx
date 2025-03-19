e1501343a226a0457f68cd36dd1678ea90cca082
Ryan Gayle <gayler02@nyu.edu>
2025-03-18 20:05:43 -0400

    updated Resume.jsx with list of proficiencies and downloadable link

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,16 @@
+export default function Resume() {
+    return (
+        <div>
+            <h1>Ryan Gayle's Proficiencies</h1>
+            <ul>
+                <li>Typescript</li>
+                <li>Javascript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>React</li>
+                <li>APIs</li>
+            </ul>
+            <p>Click on the link for the downloadable version of the resume: <a href="src\assets\Ryan_Gayle_ResumeFinal.docx" download>Ryan Gayle's Resume</a></p>
+        </div>
+    );
+};
