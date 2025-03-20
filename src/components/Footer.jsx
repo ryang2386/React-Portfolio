6d7f71133382fd259444052142991082d5dc7a26
Ryan Gayle <gayler02@nyu.edu>
2025-03-20 19:19:57 -0400

    updated CSS of Footer.jsx with import styled-components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+    background-color: var(--mint-green);
+    display: flex;
+    justify-content: center;`;
+
+const FooterLink = styled.a`
+    margin: 7px;
+    
+    img {
+        width: 35px;
+        transition: transform 0.3s, box-shadow 0.3s;
+    }
+        
+    &:hover img {
+        transform: scale(1.1);
+        box-shadow: 0 0 10px var(--paynes-gray);`
+    ;
 
 function Footer() {
-    const styles = {
-        img: {
-            width: '50px',
-        },
-    }
+    // const styles = {
+    //     img: {
+    //         width: '35px',
+    //     },
+    //     footer: {
+    //         backgroundColor: 'var(--mint-green)',
+    //         display: 'flex',
+    //         justifyContent: 'center',
+    //     },
+    //     a: {
+    //         margin: '7px',
+    //     },
+    // }
 
     return (
-        <footer>
-            <a href="https://github.com/ryang2386"><img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" style={styles.img}/></a>
-        </footer>
+        <FooterContainer>
+            <FooterLink href="https://github.com/ryang2386"><img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" alt="GitHub"/></FooterLink>
+            <FooterLink href="https://www.linkedin.com/in/ryangayle/"><img src="https://cdn-icons-png.flaticon.com/128/3536/3536505.png" alt="LinkedIn"/></FooterLink>
+            <FooterLink href="https://facebook.com/ryan.gbkja"><img src="https://cdn-icons-png.flaticon.com/128/174/174848.png" alt="Facebook"/></FooterLink>
+        </FooterContainer>
     )
 };
 
