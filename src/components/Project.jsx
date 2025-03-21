bb72cac52a9b354ea55f1664f129d91f5f60bd94
Ryan Gayle <gayler02@nyu.edu>
2025-03-21 03:10:55 -0400

    updated Project component to be used multiple times in Portfolio

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,25 +6,55 @@ const ProjectContainer = styled.div`
     flexWrap: wrap,
     justifyContent: space-around,
     backgroundColor: var(--verdigris),
+    margin: 10px;
+    position: relative;
+    width: 500px;
+    height: 200px;
 `;
 
-const ProjectLink = styled.div`
+const ProjectLink = styled.a`
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: relative;
+
     img {
         width: 500px;
+        height: 200px;
+        object-fit: cover;
         transition: opacity 0.3s ease-in-out;
-    }
+        padding: 10px;
+    };
     
     &:hover img {
         opacity: 0.8;
     }
 
+    &:hover div {
+        opacity: 1;
+    }
+}
 `;
 
-function Project( { imageURL, title }) {
+const ProjectText = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: var(--verdigris);
+    background-color: var(--paynes-gray);
+    font-size: 20px;
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+    text-align: center;
+`;
+
+function Project( { imageURL, title, link }) {
     return (
         <ProjectContainer>
-            <ProjectLink>
+            <ProjectLink href={link}>
                 <img src={imageURL} alt={title} />
+                <ProjectText>{title}</ProjectText>
             </ProjectLink>
             </ProjectContainer>
     );
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,55 +1,99 @@
-export default function Portfolio() { 
-    const styles = {
-        img: {
-            width: '500px',
-        },
-        section: {
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-around',
-            backgroundColor: 'var(--verdigris)',
-        },
-        img2: {
-            height: '266px',
-            width: '500px',
-        },
-        img4: {
-            height: '263px',
-            width: '500px',
-        },
-        h5: {
-            textAlign: 'center',
-        },
-    };
+import React from 'react';
+import Project from '../components/Project';
 
+const projects = [
+    {
+        imageURL: "/Screenshot 2025-03-18 19.05.56.png",
+        title: "Employee Database",
+        link: "https://github.com/ryang2386/Employee-Database"
+    },
+    {
+        imageURL: "/Screenshot 2025-03-18 19.03.32.png",
+        title: "README Generator",
+        link: "https://github.com/ryang2386/README-Generator"
+    },
+    {
+        imageURL: "/Screenshot 2025-03-18 18.49.36.png",
+        title: "Vehicle Builder",
+        link: "https://github.com/ryang2386/Vehicle-Builder"
+    },
+    {
+        imageURL: "/Screenshot 2025-03-18 19.09.05.png",
+        title: "Blog Website",
+        link: "https://github.com/ryang2386/Module-4"
+    },
+    {
+        imageURL: "/Screenshot 2025-03-18 19.13.31.png",
+        title: "Employee Tracker",
+        link: "https://github.com/ryang2386/Module-3"
+    },
+    {
+        imageURL: "/Screenshot 2025-03-18 19.15.25.png",
+        title: "CSS Cheat Codes Website",
+        link: "https://github.com/ryang2386/MiniProject"
+    },
+];
+
+const styles = {
+//     img: {
+//         width: '500px',
+//     },
+    div: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        backgroundColor: 'var(--verdigris)',
+    },
+//     img2: {
+//         height: '266px',
+//         width: '500px',
+//     },
+//     img4: {
+//         height: '263px',
+//         width: '500px',
+//     },
+//     h5: {
+//         textAlign: 'center',
+//     },
+};
+
+export default function Portfolio() { 
     return (
         <div style={styles.div}>
-            <section style={styles.section}>
-                <div>
-                    <a href="https://github.com/ryang2386/Employee-Database"><img src = "\Screenshot 2025-03-18 19.05.56.png" style={styles.img}/></a>
-                    <h5 style={styles.h5}>Employee Database</h5>
-                </div>
-                <div>
-                    <a href="https://github.com/ryang2386/README-Generator"><img src = "\Screenshot 2025-03-18 19.03.32.png" style={styles.img2}/></a>
-                    <h5 style={styles.h5}>README Generator</h5>
-                </div>
-                <div>
-                    <a href="https://github.com/ryang2386/Vehicle-Builder"><img src = "\Screenshot 2025-03-18 18.49.36.png" style={styles.img}/></a>
-                    <h5 style={styles.h5}>Vehicle Builder</h5>
-                </div>
-                <div>
-                    <a href="https://github.com/ryang2386/Module-4"><img src = "\Screenshot 2025-03-18 19.09.05.png" style={styles.img4}/></a>
-                    <h5 style={styles.h5}>Blog Website</h5>
-                </div>
-                <div>
-                    <a href="https://github.com/ryang2386/Module-3"><img src = "\Screenshot 2025-03-18 19.13.31.png" style={styles.img}/></a>
-                    <h5 style={styles.h5}>Employee Tracker</h5>
-                </div>
-                <div>
-                    <a href="https://github.com/ryang2386/MiniProject"><img src = "\Screenshot 2025-03-18 19.15.25.png" style={styles.img}/></a>
-                    <h5 style={styles.h5}>CSS Cheat Codes Website</h5>
-                </div>
-            </section>
+            {projects.map((project, index) => (
+                <Project key={index} link={project.link} imageURL={project.imageURL} title={project.title}/>
+            ))}
         </div>
     );
+
+    // return (
+        // <div style={styles.div}>
+        //     <section style={styles.section}>
+        //         <div>
+        //             <a href="https://github.com/ryang2386/Employee-Database"><img src = "\Screenshot 2025-03-18 19.05.56.png" style={styles.img}/></a>
+        //             <h5 style={styles.h5}>Employee Database</h5>
+        //         </div>
+        //         <div>
+        //             <a href="https://github.com/ryang2386/README-Generator"><img src = "\Screenshot 2025-03-18 19.03.32.png" style={styles.img2}/></a>
+        //             <h5 style={styles.h5}>README Generator</h5>
+        //         </div>
+        //         <div>
+        //             <a href="https://github.com/ryang2386/Vehicle-Builder"><img src = "\Screenshot 2025-03-18 18.49.36.png" style={styles.img}/></a>
+        //             <h5 style={styles.h5}>Vehicle Builder</h5>
+        //         </div>
+        //         <div>
+        //             <a href="https://github.com/ryang2386/Module-4"><img src = "\Screenshot 2025-03-18 19.09.05.png" style={styles.img4}/></a>
+        //             <h5 style={styles.h5}>Blog Website</h5>
+        //         </div>
+        //         <div>
+        //             <a href="https://github.com/ryang2386/Module-3"><img src = "\Screenshot 2025-03-18 19.13.31.png" style={styles.img}/></a>
+        //             <h5 style={styles.h5}>Employee Tracker</h5>
+        //         </div>
+        //         <div>
+        //             <a href="https://github.com/ryang2386/MiniProject"><img src = "\Screenshot 2025-03-18 19.15.25.png" style={styles.img}/></a>
+        //             <h5 style={styles.h5}>CSS Cheat Codes Website</h5>
+        //         </div>
+        //     </section>
+        // </div>
+
 };
