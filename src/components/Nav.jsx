196387147079ab6785a7fcb539c14174f2070084
Ryan Gayle <gayler02@nyu.edu>
2025-03-21 03:31:47 -0400

    updated Nav component with CSS file

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,38 +1,40 @@
 import { Link, useLocation } from 'react-router-dom';
+import './Nav.css';
 
 
 function Nav() { 
-    const styles = {
-        nav: {
-            backgroundColor: 'var(--paynes-gray)',
-            color: 'var(--mint-green)',
-            height: '50px',
-        },
-        ul: {
-            listStyleType: 'none',
-            display: 'flex',
-            justifyContent: 'flex-end',
-            padding: '10px',
-        },
-        li: {
-            padding: '10px',
-        },
-    };
+    // const styles = {
+    //     nav: {
+    //         backgroundColor: 'var(--paynes-gray)',
+    //         color: 'var(--mint-green)',
+    //         height: '50px',
+    //     },
+    //     ul: {
+    //         listStyleType: 'none',
+    //         display: 'flex',
+    //         justifyContent: 'flex-end',
+    //         padding: '10px',
+    //     },
+    //     li: {
+    //         padding: '10px',
+    //     },
+    // };
+
     const location = useLocation().pathname;
 
     return (
-        <nav style={styles.nav}>
-            <ul style={styles.ul}>
-                <li style={styles.li}>
+        <nav>
+            <ul>
+                <li>
                     <Link to="/" className={location === '/' ? 'nav-link active' : 'nav-link'}>About Me</Link>
                 </li>
-                <li style={styles.li}>
+                <li>
                     <Link to="/portfolio" className={location === '/portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</Link>
                 </li>
-                <li style={styles.li}>
+                <li>
                     <Link to="/contact" className={location === '/contact' ? 'nav-link active' : 'nav-link'}>Contact</Link>
                 </li>
-                <li style={styles.li}>
+                <li>
                     <Link to="/resume" className={location === '/resume' ? 'nav-link active' : 'nav-link'}>Resume</Link>
                 </li>
             </ul>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -35,26 +35,12 @@ const projects = [
 ];
 
 const styles = {
-//     img: {
-//         width: '500px',
-//     },
     div: {
         display: 'flex',
         flexWrap: 'wrap',
         justifyContent: 'space-around',
         backgroundColor: 'var(--verdigris)',
     },
-//     img2: {
-//         height: '266px',
-//         width: '500px',
-//     },
-//     img4: {
-//         height: '263px',
-//         width: '500px',
-//     },
-//     h5: {
-//         textAlign: 'center',
-//     },
 };
 
 export default function Portfolio() { 
@@ -65,35 +51,4 @@ export default function Portfolio() {
             ))}
         </div>
     );
-
-    // return (
-        // <div style={styles.div}>
-        //     <section style={styles.section}>
-        //         <div>
-        //             <a href="https://github.com/ryang2386/Employee-Database"><img src = "\Screenshot 2025-03-18 19.05.56.png" style={styles.img}/></a>
-        //             <h5 style={styles.h5}>Employee Database</h5>
-        //         </div>
-        //         <div>
-        //             <a href="https://github.com/ryang2386/README-Generator"><img src = "\Screenshot 2025-03-18 19.03.32.png" style={styles.img2}/></a>
-        //             <h5 style={styles.h5}>README Generator</h5>
-        //         </div>
-        //         <div>
-        //             <a href="https://github.com/ryang2386/Vehicle-Builder"><img src = "\Screenshot 2025-03-18 18.49.36.png" style={styles.img}/></a>
-        //             <h5 style={styles.h5}>Vehicle Builder</h5>
-        //         </div>
-        //         <div>
-        //             <a href="https://github.com/ryang2386/Module-4"><img src = "\Screenshot 2025-03-18 19.09.05.png" style={styles.img4}/></a>
-        //             <h5 style={styles.h5}>Blog Website</h5>
-        //         </div>
-        //         <div>
-        //             <a href="https://github.com/ryang2386/Module-3"><img src = "\Screenshot 2025-03-18 19.13.31.png" style={styles.img}/></a>
-        //             <h5 style={styles.h5}>Employee Tracker</h5>
-        //         </div>
-        //         <div>
-        //             <a href="https://github.com/ryang2386/MiniProject"><img src = "\Screenshot 2025-03-18 19.15.25.png" style={styles.img}/></a>
-        //             <h5 style={styles.h5}>CSS Cheat Codes Website</h5>
-        //         </div>
-        //     </section>
-        // </div>
-
 };
