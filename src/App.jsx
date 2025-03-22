8e7844e90bc01c63c6b6908424f581bb4f280a3d
Ryan Gayle <gayler02@nyu.edu>
2025-03-21 20:49:37 -0400

    added Project.css to make Portfolio page mobile friendly

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Outlet, useLocation } from 'react-router-dom';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import './App.css';
 
 function App() {
     const location = useLocation();
